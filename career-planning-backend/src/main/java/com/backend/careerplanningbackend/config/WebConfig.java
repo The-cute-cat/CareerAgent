cd952632abbc2b04c0793e6152bad991f05b1939
Ma Qiang <3402351070@qq.com>
2026-02-28 16:22:50 +0800

    先提交双token代码还没有测试

---
+++ career-planning-backend/src/main/java/com/backend/careerplanningbackend/config/WebConfig.java
@@ -0,0 +1,21 @@
+package com.backend.careerplanningbackend.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                .excludePathPatterns("/user/login","/user/register",
+                        "/user/send-code","/user/forget","/user/refreshToken"
+                );
+    }
+}
