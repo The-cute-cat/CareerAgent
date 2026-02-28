9e3d58a3abd5fe0738dfadcafe233e85778c5f86
Ma Qiang <3402351070@qq.com>
2026-02-28 19:26:45 +0800

    统一的Result应该改成这样子更好看一些

--- career-planning-backend/src/main/java/com/backend/careerplanningbackend/domain/po/Result.java
+++ career-planning-backend/src/main/java/com/backend/careerplanningbackend/domain/po/Result.java
@@ -13,25 +13,24 @@ public class Result<T> {
     private int code;// 成功为 --0  失败为 1
     private String msg;// 提示消息
     private T data;// 返回数据
-    private Long total;// 列表总数 (分页场景专用)
 
     /*   返回一个正确的消息   */
     public static Result ok(){
-        return new Result(200,null,null,null);
+        return new Result(200,null,null);
     }
     /*   返回一个查询到的消息   */
     public static<T> Result ok(T data){
-        return new Result(200,null,data,null);
+        return new Result(200,null,data);
     }
     /*   返回一个列表查询的正确消息   */
-    public static Result ok(List<?> data, Long total){
-        return new Result(200,null,data,total);
+    public static Result ok(List<?> data){
+        return new Result(200,null,data);
     }
     /*   返回一个错误消息   */
     public static Result fail(String msg){
-        return new Result(401,msg,null,null);
+        return new Result(401,msg,null);
     }
     public static Result fail(Integer code,String msg){
-        return new Result(code,msg,null,null);
+        return new Result(code,msg,null);
     }
 }
