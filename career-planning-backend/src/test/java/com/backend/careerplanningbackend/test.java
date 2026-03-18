bdf23234c3e31927b48c4787f149d212844a9363
Jianping Zhou <qwerty6u7i8o9p0@163.com>
2026-03-18 16:05:43 +0800

    feat: 支持MultipartFile文件上传 (#32)

--- career-planning-backend/src/test/java/com/backend/careerplanningbackend/test.java
+++ career-planning-backend/src/test/java/com/backend/careerplanningbackend/test.java
@@ -1,29 +1,11 @@
 package com.backend.careerplanningbackend;
 
-import com.backend.careerplanningbackend.domain.dto.AiChatResponse;
 import com.backend.careerplanningbackend.util.AITokenUtil;
-import com.backend.careerplanningbackend.util.AiServiceClient;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootTest
 class ATest {
-
-    @Autowired
-    private AiServiceClient client;
-
-    @Test
-    void testChatWithMessage() {
-        List<File> files = new ArrayList<>();
-        files.add(new File("C:\\Users\\The_cute_cat\\Desktop\\CareerAgent\\第十七届中国大学生服务外包创新创业大赛A13赛题.pdf"));
-        AiChatResponse result = client.chatWithFiles("/parse/files", files, "12345");
-        System.out.println(result);
-    }
 
     @Test
     void testAiToken() {
