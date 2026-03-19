529c723b260b065d2968e972739010364b32262c
dream-world37 <llkppe769sg06@163.com>
2026-03-20 01:52:08 +0800

    返回格式改为驼峰式命名

--- career-planning-ai/src/ai_service/services/struct_text_extractor.py
+++ career-planning-ai/src/ai_service/services/struct_text_extractor.py
@@ -15,7 +15,7 @@ __all__ = [
 
 class StructTextExtractor:
     def __init__(self):
-        self.llm = AIEngine().pick_brain(settings.lite_llm)
+        self.llm = AIEngine().pick_brain(settings.llm)
 
     async def extract_from_text_to_json(self, text: str) -> Any:
         response = await self.llm.set_system_role("你是一个专业的学生信息提取助手").add_text(text) \
@@ -41,21 +41,6 @@ class StructTextExtractor:
         # 对 major 进行标准化处理
         response.major = major_aligner.align(response.major)
         return json.loads(response.model_dump_json(by_alias=True))
-
-    async def extract_from_text_to_userform(self, text: str) -> Any:
-        response = await self.llm.set_system_role("你是一个专业的学生信息提取助手").add_text(text) \
-            .next_step().set_llm_params(temperature=0.1) \
-            .next_step() \
-            .into_struct(StudentFormProfile) \
-            .do()
-        if response is None:
-            log.error(f"在{__name__}中提取学生信息失败: {text}")
-            return None
-        
-        # 对 major 进行标准化处理
-        response.major = major_aligner.align(response.major)
-        return json.loads(response.model_dump_json())
-
 
 
 struct_text_extractor = StructTextExtractor()
