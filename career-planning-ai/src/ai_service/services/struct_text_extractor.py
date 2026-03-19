1a219fe272c672dad073ec0d1f681cd57eeae33a
Junpeng Le <llkppe769sg06@163.com>
2026-03-20 03:31:07 +0800

    合并 (#48)
    * 更新提取用户表单的接口与相关类，增加模糊匹配的工具类，以及增加专业的数据文件
    
    * 返回格式改为驼峰式命名
    
    * 优化有关表单提取的提示词
    
    * 返回格式改为驼峰式命名
    
    * 优化有关表单提取的提示词
    
    * 更新提取用户表单的接口与相关类，增加模糊匹配的工具类，以及增加专业的数据文件
    
    * 返回格式改为驼峰式命名
    
    * 更新提取用户表单的接口与相关类，增加模糊匹配的工具类，以及增加专业的数据文件
    
    * 返回格式改为驼峰式命名
    
    * 优化有关表单提取的提示词
    
    ---------
    
    Co-authored-by: dream-world37 <“llkppe769sg06@163.com”>

--- career-planning-ai/src/ai_service/services/struct_text_extractor.py
+++ career-planning-ai/src/ai_service/services/struct_text_extractor.py
@@ -42,5 +42,19 @@ class StructTextExtractor:
         response.major = major_aligner.align(response.major)
         return json.loads(response.model_dump_json(by_alias=True))
 
+    async def extract_from_text_to_userform(self, text: str) -> Any:
+        response = await self.llm.set_system_role("你是一个专业的学生信息提取助手").add_text(text) \
+            .next_step().set_llm_params(temperature=0.1) \
+            .next_step() \
+            .into_struct(StudentFormProfile) \
+            .do()
+        if response is None:
+            log.error(f"在{__name__}中提取学生信息失败: {text}")
+            return None
+        
+        # 对 major 进行标准化处理
+        response.major = major_aligner.align(response.major)
+        return json.loads(response.model_dump_json(by_alias=True))
+
 
 struct_text_extractor = StructTextExtractor()
