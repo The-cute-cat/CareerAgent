c5d8cc64d8377ff19e68d54397de92e881fe3017
Ma Qiang <3402351070@qq.com>
2026-03-20 20:53:28 +0800

    yweioru

--- career-planning-backend/src/main/java/com/backend/careerplanningbackend/CareerPlanningBackendApplication.java
+++ career-planning-backend/src/main/java/com/backend/careerplanningbackend/CareerPlanningBackendApplication.java
@@ -11,6 +11,6 @@ public class CareerPlanningBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(CareerPlanningBackendApplication.class, args);
     }
-    //的撒旦
+    //的撒旦hiu
 
 }
