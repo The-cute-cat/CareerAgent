9f4fea41ac6d83169e49fd7ecea610f0ddd619df
Ma Qiang <3402351070@qq.com>
2026-03-27 21:59:21 +0800

    积分消费定义类,写的比较全面

---
+++ career-planning-backend/src/main/java/com/backend/careerplanningbackend/mapper/PointsTransactionMapper.java
@@ -0,0 +1,10 @@
+package com.backend.careerplanningbackend.mapper;
+
+import com.backend.careerplanningbackend.domain.po.PointsTransaction;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface PointsTransactionMapper extends BaseMapper<PointsTransaction> {
+    
+}
